5a30a73d9362405a9617037cd811af45da996586
hwacho9 <csh77776@gmail.com>
2024-07-01 11:53:29 +0900

    feat: add entry in firebase

--- lib/screen/daily_page.dart
+++ lib/screen/daily_page.dart
@@ -1,14 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:day_stamp/providers/user_provider.dart';
-import 'package:day_stamp/screen/components/moodselector.dart';
-import 'package:day_stamp/screen/components/weatherselector.dart';
+import 'package:day_stamp/screen/components/mood_selector.dart';
+import 'package:day_stamp/screen/components/weather_selector.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import 'package:day_stamp/service/user_service.dart';
-import 'package:day_stamp/models/user_model.dart';
 import 'package:provider/provider.dart';
 
 final _firebase = FirebaseAuth.instance;
+FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 class DailyPage extends StatefulWidget {
   const DailyPage({super.key});
@@ -20,6 +20,38 @@ class DailyPage extends StatefulWidget {
 class _DailyPageState extends State<DailyPage> {
   String _selectedMood = '';
   String _selectedWeather = '';
+  TextEditingController _diaryController = TextEditingController();
+
+  void addEntry(String userId, String date, String mood, String weather,
+      List<String> tasks, String diary, List<String> emojis) {
+    var dateParts = date.split('-');
+    var year = dateParts[0];
+    var month = dateParts[1];
+
+    // Step 2: Construct the collection name dynamically
+    var collectionName = 'entries_$year$month';
+
+    firestore.collection('entries').doc(userId).collection(collectionName).add({
+      'date': date,
+      'mood': mood,
+      'weather': weather,
+      'tasks': tasks,
+      'diary': diary,
+      'emojis': emojis,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _diaryController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _diaryController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +69,7 @@ class _DailyPageState extends State<DailyPage> {
               onMoodSelected: (String mood) {
                 setState(() {
                   _selectedMood = mood;
+                  // print(_selectedMood);
                 });
               },
             ),
@@ -50,9 +83,10 @@ class _DailyPageState extends State<DailyPage> {
             ),
             const SizedBox(height: 20),
             const Text('오늘의 일기'),
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
-              child: TextField(
+              child: TextFormField(
+                controller: _diaryController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: '여기에 일기를 작성하세요',
@@ -60,6 +94,19 @@ class _DailyPageState extends State<DailyPage> {
                 maxLines: 5,
               ),
             ),
+            ElevatedButton(
+              onPressed: () {
+                addEntry(
+                    user.currentUser?.uid ?? '',
+                    DateTime.now().toString(),
+                    _selectedMood,
+                    _selectedWeather,
+                    [],
+                    _diaryController.text,
+                    []);
+              },
+              child: Text('저장'),
+            )
           ],
         ),
       ),
